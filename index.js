7be859bfa3b0bd42176664eb93ea7fabd150eda7
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-03-27 18:48:55 -0300

    chore: improved server initialization

--- index.js
+++ index.js
@@ -9,34 +9,32 @@ import { notFound } from './src/common/responses.js'
 import { connect as mongoConnect } from './src/services/mongodb.js'
 import RedisCache from './src/services/redis.js'
 
-async function run() {
-  const app = express()
+const app = express()
 
-  if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
-  }
-
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-
-  mongoConnect()
-  RedisCache.connect()
-
-  const PORT = process.env.PORT || 3333
-
-  app
-    .use(cors())
-    .use(helmet())
-    .use(logger('dev'))
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(limiter)
-    .use('/', server)
-    .use(notFound)
-
-  app.listen(PORT, () => `Server running on port ${PORT}!`)
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
 }
 
-run()
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+})
+
+mongoConnect()
+RedisCache.connect()
+
+const PORT = process.env.PORT || 3333
+
+app
+  .use(cors())
+  .use(helmet())
+  .use(logger('dev'))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(limiter)
+  .use('/', server)
+  .use(notFound)
+
+app.listen(PORT, () => `Server running on port ${PORT}!`)
+
+export default app
