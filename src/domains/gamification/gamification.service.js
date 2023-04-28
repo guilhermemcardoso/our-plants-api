0a500ec9d63514cf4bd44c70f197b6b88f3c89a7
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-04-28 15:50:42 -0300

    fix: fixed gamification service method to calculate xp to next level

--- src/domains/gamification/gamification.service.js
+++ src/domains/gamification/gamification.service.js
@@ -4,16 +4,34 @@ import { getXpToNextLevel } from './utils.js'
 
 export default class GamificationService {
   static async gamifyUserAction({ user, action }) {
-
-    const xpToNextLevel = getXpToNextLevel(user.score.level + 1)
+    let xpToNextLevel = getXpToNextLevel(user.score.level + 1)
     const actionXp = ExperiencePerAction[action]
     const totalXp = actionXp + user.score.xp
 
     const updatedScore = {
-      xp: totalXp > xpToNextLevel ? totalXp - xpToNextLevel : totalXp,
-      level: totalXp > xpToNextLevel ? user.score.level + 1 : user.score.level,
+      xp: totalXp >= xpToNextLevel ? totalXp - xpToNextLevel : totalXp,
+      level: totalXp >= xpToNextLevel ? user.score.level + 1 : user.score.level,
     }
 
-    return await User.updateById({ id: user._id, data: { score: updatedScore } })
+    if (updatedScore.level > user.score.level) {
+      xpToNextLevel = getXpToNextLevel(updatedScore.level + 1)
+    }
+
+    const updatedAuthor = await User.updateById({
+      id: user._id,
+      data: { score: updatedScore },
+    })
+
+    const userWithoutPassword = {
+      ...updatedAuthor,
+      score: { ...updatedAuthor.score, xp_next_level: xpToNextLevel },
+    }
+
+    delete userWithoutPassword.password
+    return userWithoutPassword
+  }
+
+  static getTotalXpByLevel(level) {
+    return getXpToNextLevel(level)
   }
 }
