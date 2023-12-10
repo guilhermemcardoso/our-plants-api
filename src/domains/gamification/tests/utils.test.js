8d259f20cebb989143bba1f9d7db82e63dbcc666
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-12-10 20:17:41 -0300

    test: fixed unit tests for gamification utils

--- src/domains/gamification/tests/utils.test.js
+++ src/domains/gamification/tests/utils.test.js
@@ -1,8 +1,12 @@
 import { getXpToNextLevel } from '../utils.js'
 
 describe('Gamification Utils', () => {
-  test('getXpToNextLevel should return 100 when current level is 99', () => {
-    expect(getXpToNextLevel(1)).toBe(100)
+  test('getXpToNextLevel should return 100 when current level is 2', () => {
+    expect(getXpToNextLevel(2)).toBe(100)
+  })
+
+  test('getXpToNextLevel should return 3700 when current level is 10', () => {
+    expect(getXpToNextLevel(10)).toBe(3700)
   })
 
   test('getXpToNextLevel should return 475400 when current level is 99', () => {
